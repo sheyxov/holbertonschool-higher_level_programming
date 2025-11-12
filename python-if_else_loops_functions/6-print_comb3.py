788cda9b7c064dbbac371a675eb0f2abfd45f443
sheyxov <cavansirsheyxov@gmail.com>
2025-11-12 19:52:17 +0000

    task

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(0, 10):
+        if a < b:
+            if a == 8 and  b == 9:
+                print("{}{}".format(a, b))
+            else:
+                print("{}{}, ".format(a, b), end="")
