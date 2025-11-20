a7d1d8d905defed9b382b73f34a2f14b16b8e8f6
sheyxov <cavansirsheyxov@gmail.com>
2025-11-20 20:30:18 +0000

    task

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set = []
+    result = set()
     for i in set_1:
-        for j in set_2:
-            if i != j:
-                set.append(i)
-    return set
+        if i not in set_2:
+            result.add(i)
+    for i in set_2:
+        if i not in set_1:
+            result.add(i)
+    return result
