f07db43a294d89f0c47118a4f7c1eb4fad46b5f9
sheyxov <cavansirsheyxov@gmail.com>
2025-11-20 09:07:03 +0000

    task

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-       first = None
+        first = None
     else:
-       first = sentence[0]
+        first = sentence[0]
     return (length, first)
