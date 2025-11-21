75fc472c64b85c07d4705b37b6722d1192749fd5
sheyxov <cavansirsheyxov@gmail.com>
2025-11-21 06:37:49 +0000

    task

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+         i = "{:d}".format(value)
+         print(i)
+         return True
+    except (ValueError, TypeError) as e:
+         return False
