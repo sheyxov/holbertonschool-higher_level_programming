f2f7ea04633fb0331fbea68ce099bed642518aac
sheyxov <cavansirsheyxov@gmail.com>
2025-11-27 15:11:11 +0000

    Task

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" that defines a rectangle by:"""
+"""Defines a rectangle"""
 
 
 class Rectangle:
+    """Defines a rectangle"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -32,20 +34,21 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Returns the area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """Returns the rectangle using # chars"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        rect = []
-        for i in range(self.__height):
-            rect.append("#" * self.__width)
-        return "\n".join(rect)
+        return "\n".join("#" * self.__width for _ in range(self.__height))
 
     def __repr__(self):
+        """Returns string to recreate a new instance with eval()"""
         return "Rectangle({}, {})".format(self.__width, self.__height)
