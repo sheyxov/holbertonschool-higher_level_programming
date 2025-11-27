50397f7ad45e1b071658bfdaba13a8eedecd8621
sheyxov <cavansirsheyxov@gmail.com>
2025-11-27 18:19:17 +0000

    Task

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Salam"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Salam"""
+    if not isinstance(obj, a_class):
+        return False
+    else:
+        return True
