32f196069ae6e7e12152f9dc8f8eb01a8e72889f
sheyxov <cavansirsheyxov@gmail.com>
2025-11-27 17:35:57 +0000

    Task

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,10 @@
 
 
 class MyList:
+    """
+    A custom list class that inherits from list
+    and provides a method to print the list sorted.
+    """
     def print_sorted(self):
         """ prints the list, but sorted """
         print(sorted(self))
