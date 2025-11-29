73af3f7fca709d50d25dde0521ed961ab7fe4339
sheyxov <cavansirsheyxov@gmail.com>
2025-11-29 17:06:09 +0000

    task

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
     """Salam"""
-    with open(filename, a, encoding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
