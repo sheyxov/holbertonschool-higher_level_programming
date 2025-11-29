3386001b179e97af3bf34972e7af63d44b94bd39
sheyxov <cavansirsheyxov@gmail.com>
2025-11-29 17:28:40 +0000

    task

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Student class woith filtering JSON serialization."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student object."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary representation of Student instance.
+        If attrs is a list of strings, return only attributes in that list.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        return self.__dict__
