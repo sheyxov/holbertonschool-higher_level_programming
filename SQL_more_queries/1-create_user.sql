7edad6ff3c335afb18a896db11fb9fded5bdcf27
sheyxov <cavansirsheyxov@gmail.com>
2025-11-29 19:42:52 +0000

    task

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Script to create user_0d_1 with full privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
