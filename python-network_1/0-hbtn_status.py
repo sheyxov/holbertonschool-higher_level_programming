c3ef07c77d9be8432a92da0b9ffdc542dc979391
sheyxov <cavansirsheyxov@gmail.com>
2025-12-03 13:18:50 +0000

    salam

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+"""Fetches a URL using urllib and prints the response body."""
 import urllib.request
-"""SALAM"""
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   """SALAM"""
-   html = response.read()
+
+
+def fetch_status():
+    """Fetches intranet status page and prints details."""
+    url = "https://intranet.hbtn.io/status"
+    headers = {"cfclearance": "true"}
+
+    request = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    fetch_status()
