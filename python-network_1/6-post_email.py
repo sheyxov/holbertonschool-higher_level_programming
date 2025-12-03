eaacbc8346e0e1dfab76f50a6120b420b6d2cd5d
sheyxov <cavansirsheyxov@gmail.com>
2025-12-03 14:08:55 +0000

    salam

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""SALAM"""
+import requests
+import sys
+
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+
+    response = requests.post(url, data={"email": email})
+
+    print(response.text)
