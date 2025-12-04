4c42e39fa65c167c82d6bde7f4b9df5896016f48
sheyxov <cavansirsheyxov@gmail.com>
2025-12-04 14:33:42 +0000

    task

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,39 @@
+# task_02_logic.py
+
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items_page():
+    try:
+        # JSON faylını oxuma
+        with open('items.json', 'r') as file:
+            data = json.load(file)
+            items = data.get('items', [])
+    except FileNotFoundError:
+        items = []
+
+    # Template-ə göndəririk
+    return render_template('items.html', items=items)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
